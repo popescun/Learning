74503d9ee646c651dba8fe576d1d7f6b3bf415d8
popescun <popescunicolae7@hotmail.com>
2025-09-15 22:25:46 +0200

    chore: updated Voronoi diagrams and Delaunay triangulations lab

--- coursera/Geometric_Algorithms_with_Kevin_Buchin/Voronoi_Diagrams_And_Delaunay_Triangulations/lab.py
+++ coursera/Geometric_Algorithms_with_Kevin_Buchin/Voronoi_Diagrams_And_Delaunay_Triangulations/lab.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.widgets import TextBox
@@ -30,6 +32,9 @@ class Triangle:
         self.e3 = [self.v3, self.v1]
         self.points = points
 
+    def __str__(self):
+        return f'{self.v1} {self.v2} {self.v3}'
+
     def get_edge(self, edge_no):
         return self.e1 if edge_no == 1 else self.e2 if edge_no == 2 else self.e3
 
@@ -38,6 +43,9 @@ class Triangle:
         coord1 = (self.points[edge[0]].x, self.points[edge[0]].y)
         coord2 = (self.points[edge[1]].x, self.points[edge[1]].y)
         return coord1, coord2
+
+    def vertex_coordinates(self, vertex):
+        return [self.points[vertex].x, self.points[vertex].y]
 
     def coordinates(self):
         """
@@ -66,6 +74,52 @@ class Triangle:
         y_data = (self.points[edge[0]].y, self.points[edge[1]].y)
         return x_data, y_data
 
+    def set_vertex(self, vertex, value):
+        self.v1 = value if vertex == 1 else self.v1
+        self.v2 = value if vertex == 2 else self.v2
+        self.v3 = value if vertex == 3 else  self.v3
+        self.e1 = [self.v1, self.v2]
+        self.e2 = [self.v2, self.v3]
+        self.e3 = [self.v3, self.v1]
+
+    def common_edge(self, other_t):
+        def is_same_edge(e, t: Triangle):
+            return (e[0] == t.v1 or e[0] == t.v2 or e[0] == t.v3) and (e[1] == t.v1 or e[1] == t.v2 or e[1] == t.v3)
+        # this might work but makes an assumption that exists at least a common edge to two adjacent triangles in the input data
+        # return self.e1 if self.e1 == other_t.e1 else self.e2 if self.e2 == other_t.e2 else self.e3
+        return self.e1 if is_same_edge(self.e1, other_t) else self.e2 if is_same_edge(self.e2, other_t) else self.e3 if is_same_edge(self.e3, other_t) else None
+
+    def opposite_vertices(self, other_t, common_edge):
+        e = common_edge
+        v1 = self.v1 if self.v1 not in e else self.v2 if self.v2 not in e else self.v3 if self.v3 not in e else None
+        v2 = other_t.v1 if other_t.v1 not in e else other_t.v2 if other_t.v2 not in e else other_t.v3 if other_t.v3 not in e else None
+        return [v1, v2]
+
+    def flip_edges(self, other_t):
+        c_e = self.common_edge(other_t)
+        print(f'triangles: {self}, {other_t}')
+        print(f'common edge: {c_e}')
+        if c_e is None:
+            return False
+        o_vs = self.opposite_vertices(other_t, c_e)
+        print(f'opposite vertices: {o_vs}')
+        circumcircle = triangle_circumcircle(self.coordinates())
+        def distance(a, b):
+            return math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
+        # skip if o_vs[1] is not in the circumcircle
+        if distance(circumcircle[1], self.vertex_coordinates(o_vs[1])) >= circumcircle[0]:
+            return False
+        print(f'flipping...')
+        self.set_vertex(1, o_vs[0])
+        self.set_vertex(2, o_vs[1])
+        self.set_vertex(3, c_e[1])
+        other_t.set_vertex(1, o_vs[0])
+        other_t.set_vertex(2, o_vs[1])
+        other_t.set_vertex(3, c_e[0])
+        print(f'triangles after flipping: {self}, {other_t}')
+        return True
+
+
     def draw(self, triangle_index, circumscribed: bool):
         coord = (self.points[self.v1].x + self.points[self.v2].x) / 2 + 2, (self.points[self.v1].y + self.points[self.v2].y) / 2
         ax.annotate(f't{triangle_index}', coord)
@@ -82,8 +136,9 @@ class Triangle:
         coordinates = self.coordinates()
 
         # set the axes limits before drawing the circumcircle
-        ax.set_xlim((0, max(ax.get_xlim()[1], ax.get_ylim()[1], coordinates[0].max())))
-        ax.set_ylim((0, max(ax.get_ylim()[1], ax.get_ylim()[1], coordinates[1].max())))
+        padding = 10
+        ax.set_xlim((0, max(ax.get_xlim()[1], ax.get_ylim()[1], coordinates[0].max() + padding)))
+        ax.set_ylim((0, max(ax.get_ylim()[1], ax.get_ylim()[1], coordinates[1].max() + padding)))
         
         # from https://people.math.sc.edu/Burkardt/py_src/geometry/triangle_circumcircle.py
         if circumscribed:
@@ -110,42 +165,63 @@ def read_data(filename):
             triangles.append(Triangle(t, points))
     return points, triangles
 
+points, triangles = read_data("inputTriangulation3.txt")
+
+triangles_count = min(50, len(triangles))
+
+flip_count = 0
 
 def triangulation(points: list[Point], triangles: list[Triangle], circumscribed_triangle):
     Triangle.annotations = [None] * len(triangles)
-    for i, t in enumerate(triangles[:]):
+
+    def redraw_triangles(count):
+        print('redraw_triangles...')
+        ax.clear()
+        ax.set_title(f'Triangulation({triangles_count})')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        Triangle.annotations = [None] * count
+        for i, t in enumerate(triangles[:count]):
+            t.draw(i, circumscribed_triangle == i)
+        # ax.set_aspect('equal', adjustable='box')
+        fig.canvas.draw()  # refresh
+
+    for i, t in enumerate(triangles[:triangles_count]):
         t.draw(i, circumscribed_triangle == i)
-    pass
-
-points, triangles = read_data("inputTriangulation1.txt")
-
+        for j in range(i - 1, -1, -1):
+            if t.flip_edges(triangles[j]):
+                global flip_count
+                flip_count += 1
+                redraw_triangles(i + 1)
 
 def input_changed(triangle):
     # print(triangle)
     ax.clear()
-    ax.set_title("Triangulation")
-    ax.set_xlabel("x")
-    ax.set_ylabel("y")
+    ax.set_title(f'Triangulation({triangles_count})')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
 
     triangulation(points, triangles, int(triangle))
 
     ax.set_aspect('equal', adjustable='box')
-    fig.canvas.draw()
+    fig.canvas.draw() # refresh
 
 
 # Triangulation
 
-ax.set_title("Triangulation")
-ax.set_xlabel("x")
+ax.set_title(f'Triangulation({triangles_count})')
+ax.set_xlabel('x')
 ax.set_ylabel("y")
 
 triangulation(points, triangles, 0)
 
+print(f'flip count: {flip_count}')
+
 # plt.axis("scaled") # !!! this discards xlim and ylim
 ax.set_aspect('equal', adjustable='box') # this does the trick to have equal scale
 
-box = fig.add_axes((0.3, 0.01, 0.03, 0.04))
-text_box = TextBox(box, "Triangle ", initial="0")
+box = fig.add_axes((0.35, 0.01, 0.03, 0.04))
+text_box = TextBox(box, 'Circumcircle triangle: ', initial='0')
 text_box.on_submit(input_changed)
 
 plt.show()
