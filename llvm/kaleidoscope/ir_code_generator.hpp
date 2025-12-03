8991bbfdef2623396d8d6044bf0a5276055f6149
popescun <popescunicolae7@hotmail.com>
2025-12-03 12:40:19 +0100

    chore: add doc for visitor

--- llvm/kaleidoscope/ir_code_generator.hpp
+++ llvm/kaleidoscope/ir_code_generator.hpp
@@ -11,24 +11,45 @@
 
 namespace toy {
 
-using ExpressionASTVariant =  std::variant<NumberExpressionAST,
-VariableExpressionAST,
-  BinaryExpressionAST, CallExpressionAST, FunctionPrototypeAST,
-  FunctionDefinitionAST, IfExpressionAST, ForExpressionAST>;
+using ExpressionASTVariant =
+    std::variant<NumberExpressionAST, VariableExpressionAST,
+                 BinaryExpressionAST, CallExpressionAST, FunctionPrototypeAST,
+                 FunctionDefinitionAST, IfExpressionAST, ForExpressionAST>;
 
+/**
+ * IR code generator as function object.
+ *
+ * This Functor may be used in the visitor pattern along
+ *  with std::variant & std::visit.
+ *
+ */
 struct IRCodeGenerator {
   const ParserAST &parser_ast_;
 
   explicit IRCodeGenerator(const ParserAST &parser_ast);
 
   llvm::Value *operator()(const NumberExpressionAST &expression) const;
-  llvm::Value *operator()(const VariableExpressionAST &expression) const{ return{}; }
-  llvm::Value *operator()(const BinaryExpressionAST &expression) const{ return{}; }
-  llvm::Value *operator()(const CallExpressionAST &expression) const{ return{}; }
-  llvm::Value *operator()(const FunctionPrototypeAST &expression) const{ return{}; }
-  llvm::Value *operator()(const FunctionDefinitionAST &expression) const{ return{}; }
-  llvm::Value *operator()(const IfExpressionAST &expression) const{ return{}; }
-  llvm::Value *operator()(const ForExpressionAST &expression) const{ return{}; }
+  llvm::Value *operator()(const VariableExpressionAST &expression) const {
+    return {};
+  }
+  llvm::Value *operator()(const BinaryExpressionAST &expression) const {
+    return {};
+  }
+  llvm::Value *operator()(const CallExpressionAST &expression) const {
+    return {};
+  }
+  llvm::Value *operator()(const FunctionPrototypeAST &expression) const {
+    return {};
+  }
+  llvm::Value *operator()(const FunctionDefinitionAST &expression) const {
+    return {};
+  }
+  llvm::Value *operator()(const IfExpressionAST &expression) const {
+    return {};
+  }
+  llvm::Value *operator()(const ForExpressionAST &expression) const {
+    return {};
+  }
 };
 
 } // namespace toy
