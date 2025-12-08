77f7570a4110a024d2efdc66198beac584f56243
popescun <popescunicolae7@hotmail.com>
2025-12-08 11:48:00 +0100

    move project to separate repo

--- llvm/kaleidoscope/README.md
+++
@@ -1,3 +0,0 @@
-# Implementing a language with LLVM
-## References
-- [My First Language Frontend with LLVM](https://llvm.org/docs/tutorial/MyFirstLanguageFrontend/index.html#my-first-language-frontend-with-llvm-tutorial)
--- llvm/kaleidoscope/visualize_callgraph.sh
+++
@@ -1,6 +0,0 @@
-~/github/llvm-project/llvm/install/bin/llvm-as  < t.ll >> t.as
-~/github/llvm-project/llvm/install/bin/opt --passes=dot-callgraph t.as >> t.dot 
-mv t.as.callgraph.dot t.dot
-rm t.as
-
-dot -Tpng t.dot >> t.png && open t.png
