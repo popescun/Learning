dcf1f1c476f3b977a3ae4718449391bfa16a145e
popescun <popescunicolae7@hotmail.com>
2026-03-04 11:42:37 +0100

    chore: update formulas and python requirements

--- Delft/Aircraft_Performance_Pyhysics_and _imulation/equations.py
+++ Delft/Aircraft_Performance_Pyhysics_and _imulation/equations.py
@@ -63,6 +63,7 @@ l = lambda cl, h, v, s: cl * p_dynamic(h, v) * s
 
 # Lift coefficient
 cl = lambda l, h, v, s: l / (p_dynamic(h, v) * s)
+cl_by_density = lambda l, d, v, s: 2 * l / (d*pow(v, 2)*s)
 
 # The drag
 """
@@ -73,6 +74,7 @@ cl = lambda l, h, v, s: l / (p_dynamic(h, v) * s)
         s  - wing surface [m2] 
 """
 dr = lambda cd, h, v, s: cd * p_dynamic(h, v) * s
+dr_by_density = lambda cd, d, v, s: cd * d * pow(v, 2) * s / 2
 
 """
     Wing aspect ratio
@@ -92,6 +94,7 @@ ar = lambda b, s: pow(b, 2) / s
 """
 id = lambda  cl, b, s, e: pow(cl, 2) / (math.pi * ar(b, s) * e)
 cd = lambda cd0, cl, b, s, e: cd0 + id(cl, b, s, e)
+cd_with_k = lambda cd0, cl, k: cd0 + k*pow(cl, 2)
 
 # Propulsion
 """
@@ -202,4 +205,87 @@ v_min = lambda w, s, d, cl: math.sqrt(2 * w / (s * d * cl))
         d  - air density [Kg/m3]
         cl - lift coefficient  
 """
-v_max = lambda w, s, d, cl: math.sqrt(w/s * 2 /d * 1 / cl)
+v_max = lambda w, s, d, cl: math.sqrt(w / s * 2 /d * 1 / cl)
+
+# climb and descent
+"""
+  Climb rate [m/s]
+    v - aircraft true speed [m/s] 
+    gamma - climb angle [degree]
+"""
+rc = lambda v, gamma: v * math.sin(gamma * math.pi / 180)
+
+"""
+    Climb distance [m]
+        v - aircraft true speed [m/s] 
+        gamma - climb angle [degree]
+"""
+dc = lambda v, gamma: v * math.cos(gamma * math.pi / 180)
+
+"""
+    Climb angle [degree]
+      t - thrust [N]
+      cd - drag coefficient
+      d - air density [Kg/m3]
+      v - aircraft true speed [m/s] 
+      s - wing surface [m2]
+      w - weight [Kg]
+      h - altitude [m]
+"""
+gamma = lambda t, cd, v, s, w, h: math.asin((t - dr(cd, h, v, s)) / w)
+gamma_by_density = lambda t, cd, d, v, s, w: math.asin((t - dr_by_density(cd, d, v, s)) / w)
+
+"""
+    Climb rate by power [m/s]
+        pa - power available power [W]
+        pr - power required [W]
+        w - aircraft weight [Kg]
+"""
+rc_by_power = lambda pa, pr, w: (pa - pr) / w
+
+"""
+    Climb rate by forces [m/s]
+        t  - thrust [N]
+        dr - drag [N] 
+        w  - aircraft weight [Kg]
+        v  - aircraft true speed [m/s] 
+"""
+rc_by_forces = lambda t, dr, w, v: (t - dr) / w * v
+
+""" 
+    True air speed [m/s]
+        v_eas - estimated airspeed [m/s]
+        d     - air density [Kg/m3]
+"""
+v_tas  = lambda v_eas, d: math.sqrt(D0 / d) * v_eas
+
+"""
+    Change of true air speed(rate) with altitude [m^-1]
+        v_eas - estimated airspeed [m/s]
+        h1    - first altitude [m]
+        h2    - second altitude [m]
+"""
+delta_v_tas = lambda v_eas, h1, h2: v_eas * (math.sqrt(D0 / d(h2)) - math.sqrt(D0 / d(h1))) / (h2 - h1)
+
+""" 
+    Climb rate ratio (rc / rc_steady)
+        v     - true airspeed [m/s] at altitude h1
+        v_eas - estimated airspeed [m/s]
+        h1    - first altitude [m]
+        h2    - second altitude [m]
+"""
+rc_r = lambda v, v_eas, h1, h2: 1 / (1 + v / G * delta_v_tas(v_eas, h1, h2))
+
+"""
+    Glider minimum glide angle [degree]
+        v_eas - estimated airspeed [m/s]
+"""
+gamma_glide_min = lambda cl, cd: math.asin(cd / cl) * 180 / math.pi
+
+""" 
+    Time to climb [s] 
+        h1 - first altitude [m]
+        h2 - second altitude [m]
+        rc - climb rate [m/s] 
+"""
+ct = lambda h1, h2, rc: (h2 - h1) / rc
