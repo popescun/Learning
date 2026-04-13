fc12a9339da8a6073e6eb3290f70fdbe80865a39
popescun <popescunicolae7@hotmail.com>
2026-04-13 13:59:10 +0200

    feat: add cmake project for building for STM32F4XX

---
+++ udemy/Advanced_Embedded_Software_with_STM32_FreeRTOS_and_Modbus/CLion/rtos_project/config/mcu.h
@@ -0,0 +1,12 @@
+/*
+* mcu.h
+ *
+ * Centralized inclusion for the MCU peripheral access layer for the STM32F series.
+ */
+
+#ifndef MCU_H_
+#define MCU_H_
+
+#include <stm32f4xx.h>
+
+#endif /* MCU_H_ */
